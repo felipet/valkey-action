538bdc597e0475e7abb5984a529a2e73223b2d4b
Felipe Torres González <torresfelipex1@gmail.com>
2025-07-08 17:50:12 +0200

    Add main shell script

---
+++ script/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -euo pipefail
+
+binary="docker"
+
+cmd_run="$binary run -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT docker.io/valkey/valkey:$VALKEY_VERSION"
+
+CONTAINER_NAME=$(eval "$cmd_run")
+
+echo "Starting the container $CONTAINER_NAME ..."
+
+while true; do
+    STATUS=$($binary inspect --format='{{.State.Status}}' "$CONTAINER_NAME" 2>/dev/null)
+    
+    if [ "$STATUS" == "running" ]; then
+        echo "The container $CONTAINER_NAME is up and running!"
+        break
+    elif [ "$STATUS" == "exited" || "$STATUS" == "dead" ]; then
+        echo "Failed to start the container $CONTAINER_NAME"
+        exit 1
+    else
+        echo "The container $CONTAINER_NAME is still starting"
+        sleep 1
+    fi 
+done
+
+exit 0
